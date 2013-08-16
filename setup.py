1b3339c069f2cfd6dbfcd74b18ae30c2eb9a694b
Troytft <troytft@gmail.com>
2013-08-16 21:55:33 +0400

    Initial

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup_requires = [
+    'django',
+]
+
+setup(
+    name='bumple-downloader',
+    version='0.1dev',
+    packages=['bumple-downloader',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+)
